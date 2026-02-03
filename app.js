bf7c6e7c5ac89b842cda710fb10a54bdca848e5b
sahilb01 <sahilbhimla@gmail.com>
2026-02-03 21:32:39 +0530

    Use process.env.PORT for Render

--- app.js
+++ app.js
@@ -113,7 +113,7 @@ app.use((err,req,res,next)=>{
     res.status(status).render('error.ejs',{message})
     // res.status(status).send(message);
 })
-const PORT=8080;
+const PORT=process.env.PORT || 8080;
 
 app.listen(PORT,()=>{
     console.log("Server is running on port http://localhost:8080/listings");
